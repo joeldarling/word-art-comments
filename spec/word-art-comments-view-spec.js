33f0d9910a9f48e215dd3d9d4a2a4ad9320c0204
Joel <joel.darling@gmail.com>
2016-09-10 09:45:35 -0400

    initial commit

---
+++ spec/word-art-comments-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import WordArtCommentsView from '../lib/word-art-comments-view';
+
+describe('WordArtCommentsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
